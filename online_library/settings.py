17dbda2cf7f5312a70ed969cad3e8f9780914ef6
Tasara-Limited <tasarait@gmail.com>
2026-03-21 02:43:03 +0600

    Set STATIC_ROOT for Render

--- online_library/settings.py
+++ online_library/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
